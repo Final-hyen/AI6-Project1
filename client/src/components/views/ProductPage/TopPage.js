64836dff5d660227e70695eb6c62848825d2afe8
Final Hyen <jwx9797@gmail.com>
2023-09-18 23:27:00 +0900

    feat : 카테고리별 페이지 생성

--- client/src/components/views/ProductPage/TopPage.js
+++ client/src/components/views/ProductPage/TopPage.js
@@ -1,10 +1,14 @@
-import React from "react";
-import {ProductList} from "../../Products/list"
+import React, { useEffect, useState } from "react";
+import { axiosClient } from "../../../utils/axiosClient";
 
+const GetTopProduct = () => {
+  const [tops, setTops] = useState({});
 
-export default function TopPage() {
-  return(
-    <div>
-      <ProductList/>
-    </div>
-  )};
+  useEffect(() => {
+    axiosClient.get("/products").then((res) => {
+      if (res.data.category === "top") return setTops(res.data);
+    }).catch(e => console.log(e));
+  });
+};
+
+export default GetTopProduct
