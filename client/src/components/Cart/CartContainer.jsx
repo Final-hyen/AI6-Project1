79c21928404ef32e8bc4218462250e46bc54b4be
Final Hyen <jwx9797@gmail.com>
2023-10-11 17:34:23 +0900

    refactor : CartPage 체크박스 전체  체크/해제 구현

--- client/src/components/Cart/CartContainer.jsx
+++ client/src/components/Cart/CartContainer.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import CartPresentation from "./CartPresentation";
 
 const CartContainer = () => {
     const localData = localStorage.getItem('cartItems')
     const cartItems = JSON.parse(localData)
+    const [isChecked, setIsChecked] = useState(false);
 
-    return <CartPresentation cartItems={cartItems} />
+    const totalPrice = cartItems.reduce((a,b) => a+  b.price, 0)
+    
+    const isClickCheckAll = () => {
+        setIsChecked(!isChecked)
+    }
+
+    return <CartPresentation cartItems={cartItems} totalPrice={totalPrice} isChecked={isChecked} isClickCheckAll={isClickCheckAll}/>
 }
 
 export default CartContainer;
--- client/src/components/Cart/CartPresentation.jsx
+++ client/src/components/Cart/CartPresentation.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Table } from "./CartCSS";
 import { MyPageNav } from "../Mypage/MypageNav";
 
-const CartPresentation = ({ cartItems }) => {
+const CartPresentation = ({
+  cartItems,
+  totalPrice,
+  isChecked,
+  isClickCheckAll,
+}) => {
   return (
     <>
       <MyPageNav />
@@ -10,6 +15,14 @@ const CartPresentation = ({ cartItems }) => {
         <table>
           <thead>
             <tr>
+              <td>
+                <input
+                  id="checkAll"
+                  type="checkbox"
+                  onChange={isClickCheckAll}
+                  checked={isChecked}
+                />
+              </td>
               <td>Product Img</td>
               <td>Product Name</td>
               <td>Count</td>
@@ -19,6 +32,14 @@ const CartPresentation = ({ cartItems }) => {
           <tbody>
             {cartItems.map((item, idx) => (
               <tr key={idx}>
+                <td>
+                  <input
+                    id="chk"
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={isClickCheckAll}
+                  />
+                </td>
                 <td>
                   <img src={item.imgUrl} alt="제품 사진" />
                 </td>
@@ -31,7 +52,10 @@ const CartPresentation = ({ cartItems }) => {
               <td>
                 <button>Delete</button>
               </td>
-              <td>Total원</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>{totalPrice}원</td>
             </tr>
           </tbody>
         </table>
