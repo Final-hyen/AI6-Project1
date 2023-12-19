e62b11320592e400d017547f4266192da05928ac
Final Hyen <jwx9797@gmail.com>
2023-12-19 22:50:19 +0900

    fix: OrderInfo 데이터 날짜별로 나누기 코드 오류 수정

--- client/src/components/Admin/OrderInfoGetContain.jsx
+++ client/src/components/Admin/OrderInfoGetContain.jsx
@@ -4,68 +4,39 @@ import OrderGetPresent from "./OrderInfoGetPresent";
 
 const OrderGetContain = () => {
   const [orderUserInfo, setOrderUserInfo] = useState([]);
-  let ordersInfo = [];
-  let orders = [];
 
   useEffect(() => {
-    axiosClient
-      .get(`/orders`)
-      .then((res) => {
-        setOrderUserInfo(res.data.order);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [setOrderUserInfo]);
+    let isCleanUp = true;
+    const fetchData = async () => {
+      try {
+        const res = await axiosClient.get(`/orders`);
+        if (isCleanUp && res?.status === 200) {
+          setOrderUserInfo(res.data.order);
+        };
+      } catch (err) {
+        console.error(err);
+      };
+    };
+    fetchData();
+    return () => {
+      isCleanUp = false;
+    };
+}, []);
 
-  orderUserInfo.map((order) =>
-    ordersInfo.push([order.product_info, order.create_at])
-  );
-  // 원하는 정보를 뽑았다!
-  // for (let i = 0; i < ordersInfo.length; i++){
-  //   for(let j = 0; j < ordersInfo[i].length; j++){
-  //     orders.push(ordersInfo[i][j])
-  //   }
-  // }
-  //console.log(ordersInfo[0][0], ordersInfo[0][1]);
-
-  ordersInfo.map((order) => {
-    const dateString = new Date(order[1]);
+  orderUserInfo.map((order) => {
+    const dateString = new Date(order['create_at']);
     const year = dateString.getFullYear();
     const month = dateString.getMonth() + 1;
     const day = dateString.getDay();
     const hour = dateString.getHours();
     const min = dateString.getMinutes();
     const date = `${year}년 ${month}월 ${day}일 ${hour}시 ${min}분`;
-    order[1] = date;
+    order['create_at'] = date;
   });
-  console.log("ordersInfo", ordersInfo);
-  const a = [];
-  ordersInfo.map((orders) => orders.map((order) => a.push(order)));
-  console.log("a", a);
-
-  function divideDataByDate(data) {
-    let currentDate = null;
-
-    const dividedData = data
-      .filter((item) => {
-        if (typeof item === "string") {
-          currentDate = item
-          return false;
-        } else {
-          return true;
-        }
-      })
-      .map((item) => {
-        return { date: currentDate, data: item };
-      });
-
-    return dividedData;
-  }
-  console.log('b',divideDataByDate(a));
+  
   return (
     <StrictMode>
-      <OrderGetPresent ordersInfo={ordersInfo} orders={orders} />
+      <OrderGetPresent ordersInfo={orderUserInfo} />
     </StrictMode>
   );
 };
--- client/src/components/Admin/OrderInfoGetPresent.jsx
+++ client/src/components/Admin/OrderInfoGetPresent.jsx
@@ -1,41 +1,43 @@
 import React from "react";
 import { Button, Caption, Img, Table, Td, Th } from "./ProductGetCSS";
 
-const OrderGetPresent = ({ ordersInfo, orders}) => {
+const OrderGetPresent = ({ ordersInfo }) => {
   return (
     <>
-    <h2>Order Tracking</h2>
-    {ordersInfo.map((date, idx) => (
-      <Table key={idx}>
-      <Caption>{date[1]}</Caption>
-      <thead>
-        <tr>
-          <Th>제품 사진</Th>
-          <Th>제품 이름</Th>
-          <Th>주문 수량</Th>
-          <Th>제품 가격</Th>
-          <Th>Order Control</Th>
-        </tr>
-      </thead>
-      <tbody>
-        {ordersInfo.map((order,idx) => (
-          <tr key={idx}>
-            <Td id="img">
-              <Img src={order['imgUrl']} alt="주문 제품 이미지" />
-            </Td>
-            <Td id="title">{order['title']}</Td>
-            <Td id ="count">{order.count}</Td>
-            <Td id="price">{order.price}원</Td>
-            <Td>
-              <Button>배송 준비 중</Button>
-              <Button>배송 중</Button>
-              <Button>배송 완료</Button>
-            </Td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-    ))}
+      <h2>Order Tracking</h2>
+      {ordersInfo.map((orders, idx) => (
+        <Table key={idx}>
+          <Caption>{orders['create_at']}</Caption>
+          <thead>
+            <tr>
+              <Th>제품 사진</Th>
+              <Th>제품 이름</Th>
+              <Th>주문 수량</Th>
+              <Th>제품 가격</Th>
+              <Th>Order Control</Th>
+            </tr>
+          </thead>
+          <tbody>
+              {orders['product_info'].map((order, orderIdx) => (
+                <tr key={orderIdx}>
+                  <Td id="img">
+                    <Img src={order["imgUrl"]} alt="주문 제품 이미지" />
+                  </Td>
+                  <Td id="title">{order["title"]}</Td>
+                  <Td id="count">{order.count}</Td>
+                  <Td id="price">{order.price}원</Td>
+                  <Td>
+                    <Button>배송 준비 중</Button>
+                    <Button>배송 중</Button>
+                    <Button>배송 완료</Button>
+                  </Td>
+                </tr>
+              ))
+            }
+            
+          </tbody>
+        </Table>
+      ))}
     </>
   );
 };
