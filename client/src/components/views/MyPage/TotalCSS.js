9fb87e62a8241111b14ed6660f583709e3f4860b
Final Hyen <jwx9797@gmail.com>
2024-01-25 16:10:12 +0900

    style: Total CSS

---
+++ client/src/components/views/MyPage/TotalCSS.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    border-left: solid grey;
+    border-right: solid grey;
+    border-radius: 50px;
+    margin: 30px;
+    
+    .date {
+        margin-left: 80px;
+        margin-top: 30px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+`
+
+export const Status = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+export const Descriptions = styled.div`
+    width: 800px;
+    font-size: 30px;
+    display: flex;
+    flex-direction : column;
+`
+export const ProductImg = styled.img`
+    width: 250px;
+    margin: 40px;
+`
+
+export const Button = styled.button`
+    width: 400px;
+    margin: auto;
+`
--- client/src/components/views/MyPage/TotalTrackingPresnt.jsx
+++ client/src/components/views/MyPage/TotalTrackingPresnt.jsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { OrderStatus} from "./OrderTrackingCSS";
+import {
+  Container,
+  Status,
+  ProductImg,
+  Descriptions,
+  Button,
+} from "./TotalCSS";
 
 const TotalTrackingPresentation = ({ orders }) => {
   return (
     <>
       <h3>[ 주문/배송 조회 ]</h3>
       {orders.map((order, idx) => (
-        <OrderStatus key={idx}>
-           <span>{order.create_at}</span>
-           {order.product_info.map((product,idx) =>(
-            <div key={idx}>
-                <img src={product.imgUrl} alt="Product Img"/>
-            </div>
-           ))}
-        </OrderStatus>
+        <Container key={idx}>
+          <span className="date">{order.create_at}</span>
+          {order.product_info.map((product, idx) => (
+            <>
+              <Status key={idx}>
+                <ProductImg
+                  className="img"
+                  src={product.imgUrl}
+                  alt="Product Img"
+                />
+                <Descriptions>
+                  <span>Title : {product.title}</span>
+                  <span>Price : {product.price}</span>
+                </Descriptions>
+              </Status>
+              <hr />
+            </>
+          ))}
+          <Button>배송 조회</Button>
+        </Container>
       ))}
-      
     </>
   );
 };
