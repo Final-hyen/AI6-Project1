0beaeead42877fec4ac4a5a604076924d7c1139a
Final Hyen <jwx9797@gmail.com>
2024-02-28 16:54:09 +0900

    style: Image Lazy Loading & blur

--- client/src/components/views/ProductPage/ProductPresentation.jsx
+++ client/src/components/views/ProductPage/ProductPresentation.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Contain, Figure, Grid, Img } from "./ProductPageCSS";
 import { H1 } from "../../Admin/ProductGetCSS";
 
 const ProductPresentation = ({ products, category }) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.dataset.src;
+          img.classList.remove("lazy");
+          img.style.filter = "none";
+          observer.unobserve(img);
+        }
+      });
+    });
+    const images = document.querySelectorAll(".lazy");
+    images.forEach((img) => observer.observe(img));
+
+    return () => {
+      images.forEach(img => observer.unobserve(img))
+    }
+  },[products])
   return (
     <Contain>
       <H1>{category} Products</H1>
@@ -10,7 +29,7 @@ const ProductPresentation = ({ products, category }) => {
         {products.map((product, idx) => (
           <Figure key={idx}>
             <a href={`/detail/${product._id}`}>
-              <Img src={product.imgUrl} alt="제품 이미지" />
+              <Img className="productItem lazy" data-src={product.imgUrl} alt="제품 이미지" />
               <p>{product.company}</p>
               <p>{product.title}</p>
               <p>{product.price}</p>
